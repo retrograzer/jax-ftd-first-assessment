6f01e4e6b053e73202bdfe0d29d07a17ddcd1561
NoahSchultz <noahgschultz@gmail.com>
2016-06-30 16:37:35 -0400

    fixes strange bug in fileDao

--- java-server/src/main/java/com/cooksys/ftd/assesment/dao/FilesDao.java
+++ java-server/src/main/java/com/cooksys/ftd/assesment/dao/FilesDao.java
@@ -6,30 +6,33 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
+import com.cooksys.ftd.assessment.model.db.Files;
+
 public class FilesDao extends AbstractDao {
 	
 	AbstractDao abs = new AbstractDao();
 	Connection conn = abs.getConn();
 	
-	try {
+	public Files createFiles () {
 		
-		String sql = "select * "
-				+ "from user";
-		PreparedStatement stmt = conn.prepareStatement(sql);
-		
-		ResultSet rs = stmt.executeQuery();
-
-		log.info("Retrieving actors...");
-		while (rs.next()) {
-			String firstName = rs.getString("username");
-			String lastName = rs.getString("password");
-			log.info("{} {}", firstName, lastName);
+		try {
+			String sql = "select * "
+					+ "from user";
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			
+			ResultSet rs = stmt.executeQuery();
+	
+			log.info("Retrieving actors...");
+			while (rs.next()) {
+				String firstName = rs.getString("username");
+				String lastName = rs.getString("password");
+				log.info("{} {}", firstName, lastName);
+			}
+			
+		} catch (SQLException e) {
+			log.error("Something went wrong :/", e);
 		}
-		
-	} catch (SQLException e) {
-		log.error("Something went wrong :/", e);
-	}
-		return user;
+		return null;
 	}
 	
 	public <E> List<E> FileList () {
